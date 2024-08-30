08be1d527f71386c79ee348886eba8790c8392d7
nxocorp <nxocorp@gmail.com>
2024-08-29 22:29:08 -0400

    tailwind installed

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - npm create svelte@latest
 - npm install
 - npm run dev
+
+### Install tailwindcss
+- npm install tailwindcss postcss autoprefixer
+- npx tailwindcss init -p
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import "../app.css";
+    let welcome = "Welcome!! ";
+</script>
+  
+<h1>{welcome} to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<slot />
